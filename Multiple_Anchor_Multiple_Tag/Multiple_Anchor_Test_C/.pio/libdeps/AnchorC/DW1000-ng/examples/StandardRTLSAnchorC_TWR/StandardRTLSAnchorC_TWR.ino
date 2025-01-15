5c422dc9edd18da7564f8c216ef83e7f918a20c1
jianhuixin <comet112511092.ee12@nycu.edu.tw>
2025-01-15 23:12:53 +0800

    end of 1/15's working

--- Multiple_Anchor_Multiple_Tag/Multiple_Anchor_Test_C/.pio/libdeps/AnchorC/DW1000-ng/examples/StandardRTLSAnchorC_TWR/StandardRTLSAnchorC_TWR.ino
+++ Multiple_Anchor_Multiple_Tag/Multiple_Anchor_Test_C/.pio/libdeps/AnchorC/DW1000-ng/examples/StandardRTLSAnchorC_TWR/StandardRTLSAnchorC_TWR.ino
@@ -1,30 +1,36 @@
+/*
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+/* 
+ * StandardRTLSAnchorC_TWR.ino
+ * 
+ * This is an example slave anchor in a RTLS using two way ranging ISO/IEC 24730-62_2013 messages
+ */
+
+#include <SPI.h>
 #include <DW1000Ng.hpp>
 #include <DW1000NgUtils.hpp>
 #include <DW1000NgRanging.hpp>
 #include <DW1000NgRTLS.hpp>
 
-typedef struct Position {
-    double x;
-    double y;
-} Position;
-
 // connection pins
 #if defined(ESP8266)
 const uint8_t PIN_SS = 15;
-const uint8_t PIN_RST = 16;
-const uint8_t PIN_IRQ = 5;
-#elif defined(ESP32)
-const uint8_t PIN_RST = 15;
+#else
+const uint8_t PIN_RST = 9;
 const uint8_t PIN_SS = SS; // spi select pin
 #endif
 
-byte main_anchor_address[] = {0x01, 0x00};
-
-uint16_t next_anchor = 3;
-
 double range_self;
 
-byte currentTagEUI[8]; // Array to store the tag's EUI (8 bytes)
+byte main_anchor_address[] = {0x01, 0x00};
+
+uint16_t blink_rate = 200;
 
 device_configuration_t DEFAULT_CONFIG = {
     false,
@@ -48,35 +54,34 @@ frame_filtering_configuration_t ANCHOR_FRAME_FILTER_CONFIG = {
     false,
     false,
     false,
-    true /* This allows blink frames */
-};
-
-interrupt_configuration_t DEFAULT_INTERRUPT_CONFIG = {
-    true,
-    true,
-    true,
-    false,
-    true
+    false
 };
 
 void setup() {
-    delay(5000);
-    Serial.begin(9600);
-    Serial.println(F("### arduino-DW1000Ng-ranging-anchor-B ###"));
+    // DEBUG monitoring
+    Serial.begin(115200);
+    Serial.println(F("### arduino-DW1000Ng-ranging-anchor-C ###"));
+    // initialize the driver
+    #if defined(ESP8266)
+    DW1000Ng::initializeNoInterrupt(PIN_SS);
+    #else
     DW1000Ng::initializeNoInterrupt(PIN_SS, PIN_RST);
+    #endif
     Serial.println(F("DW1000Ng initialized ..."));
+    // general configuration
     DW1000Ng::applyConfiguration(DEFAULT_CONFIG);
     DW1000Ng::enableFrameFiltering(ANCHOR_FRAME_FILTER_CONFIG);
-    DW1000Ng::setEUI("AA:BB:CC:DD:EE:FF:00:02");
-    DW1000Ng::setNetworkId(RTLS_APP_ID);
-
-    DW1000Ng::setAntennaDelay(16436);
-
-    DW1000Ng::applyInterruptConfiguration(DEFAULT_INTERRUPT_CONFIG);
+    
+    DW1000Ng::setEUI("AA:BB:CC:DD:EE:FF:00:03");
 
     DW1000Ng::setPreambleDetectionTimeout(64);
     DW1000Ng::setSfdDetectionTimeout(273);
     DW1000Ng::setReceiveFrameWaitTimeoutPeriod(5000);
+
+    DW1000Ng::setNetworkId(RTLS_APP_ID);
+    DW1000Ng::setDeviceAddress(3);
+	
+    DW1000Ng::setAntennaDelay(16436);
     
     Serial.println(F("Committed configuration ..."));
     // DEBUG chip info and registers pretty printed
@@ -88,9 +93,7 @@ void setup() {
     DW1000Ng::getPrintableNetworkIdAndShortAddress(msg);
     Serial.print("Network ID & Device Address: "); Serial.println(msg);
     DW1000Ng::getPrintableDeviceMode(msg);
-    Serial.print("Device mode: "); Serial.println(msg);   
-    DW1000Ng::applyInterruptConfiguration(DEFAULT_INTERRUPT_CONFIG);
-    delay(5000); 
+    Serial.print("Device mode: "); Serial.println(msg);
 }
 
 void transmitRangeReport() {
@@ -99,28 +102,20 @@ void transmitRangeReport() {
     memcpy(&rangingReport[5], main_anchor_address, 2);
     DW1000Ng::getDeviceAddress(&rangingReport[7]);
     DW1000NgUtils::writeValueToBytes(&rangingReport[10], static_cast<uint16_t>((range_self*1000)), 2);
-    memcpy(&rangingReport[16], currentTagEUI, 8); // Add tag EUI to the report
     DW1000Ng::setTransmitData(rangingReport, sizeof(rangingReport));
     DW1000Ng::startTransmit();
-    Serial.println("Transmitting range report");
 }
-
-void loop() {  
-    RangeAcceptResult result = DW1000NgRTLS::anchorRangeAccept(NextActivity::RANGING_CONFIRM, next_anchor);
-    if(result.success) {
-        delay(2); // Tweak based on your hardware
+ 
+void loop() {
+     RangeAcceptResult result = DW1000NgRTLS::anchorRangeAccept(NextActivity::ACTIVITY_FINISHED, blink_rate);
+     if(result.success) {
+        delay(1); // Tweak based on your hardware
         range_self = result.range;
-
-        // Get the tag EUI from the received data
-        size_t recv_len = DW1000Ng::getReceivedDataLength();
-        byte recv_data[recv_len];
-        DW1000Ng::getReceivedData(recv_data, recv_len);
-        memcpy(currentTagEUI, &recv_data[2], 8); // EUI starts at position 2 (assuming EUI is 8 bytes long)
-
         transmitRangeReport();
 
         String rangeString = "Range: "; rangeString += range_self; rangeString += " m";
         rangeString += "\t RX power: "; rangeString += DW1000Ng::getReceivePower(); rangeString += " dBm";
         Serial.println(rangeString);
-    }
+     }
 }
+
