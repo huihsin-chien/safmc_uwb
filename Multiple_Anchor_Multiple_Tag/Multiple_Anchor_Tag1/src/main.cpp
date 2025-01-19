555517b175be0541293f6463f0dce07ee35af680
jianhuixin <comet112511092.ee12@nycu.edu.tw>
2025-01-19 20:05:55 +0800

    delet unused code

--- Multiple_Anchor_Multiple_Tag/Multiple_Anchor_Tag1/src/main.cpp
+++ Multiple_Anchor_Multiple_Tag/Multiple_Anchor_Tag1/src/main.cpp
@@ -25,10 +25,7 @@ frame_filtering_configuration_t TAG_FRAME_FILTER_CONFIG = {
     false, false, true, false, false, false, false, false
 };
 
-frame_filtering_configuration_t ANCHOR_FRAME_FILTER_CONFIG = {
-    false, false, true, false, false, false, false,
-    true /* This allows blink frames */
-};
+
 
 sleep_configuration_t SLEEP_CONFIG = {
     false,  // onWakeUpRunADC   reg 0x2C:00
@@ -41,14 +38,7 @@ sleep_configuration_t SLEEP_CONFIG = {
     true    // enableWakeSPI
 };
 
-interrupt_configuration_t DEFAULT_INTERRUPT_CONFIG = {
-    true,   // interruptOnSent
-    true,   // interruptOnReceived
-    false,  // interruptOnReceiveFailed
-    false,  // interruptOnReceiveTimeout
-    false,  // interruptOnReceiveTimestampAvailable
-    false   // interruptOnAutomaticAcknowledgeTrigger
-};
+
 
 byte tag_short_address[] = {0x01, 0x01}; // 設定當前 tag 的短地址
 byte main_anchor_address[] = {0x01, 0x00};
@@ -109,16 +99,6 @@ void setup() {
     DW1000Ng::setGPIOMode(12,   LED_MODE);
 
     delay(5000); // 等待 5 秒
-    // Serial.begin(9600);
-    // Serial.println(F("### arduino-DW1000Ng-ranging-tag ###"));
-    // DW1000Ng::initializeNoInterrupt(PIN_SS, PIN_RST);
-    // Serial.println(F("DW1000Ng initialized ..."));
-    // DW1000Ng::applyConfiguration(DEFAULT_CONFIG);
-    // DW1000Ng::enableFrameFiltering(ANCHOR_FRAME_FILTER_CONFIG);
-    // DW1000Ng::setEUI("AA:BB:CC:DD:EE:FF:00:04");
-    // DW1000Ng::setPreambleDetectionTimeout(64);
-    // DW1000Ng::setSfdDetectionTimeout(273);
-    // DW1000Ng::applyInterruptConfiguration(DEFAULT_INTERRUPT_CONFIG);
 }
 
 void loop() {
@@ -134,38 +114,3 @@ void loop() {
         blink_rate = res.new_blink_rate;
     }
 }
-//     if (DW1000NgRTLS::receiveFrame()) {
-//         size_t recv_len = DW1000Ng::getReceivedDataLength();
-//         byte recv_data[recv_len];
-//         DW1000Ng::getReceivedData(recv_data, recv_len);
-
-//         // 檢查數據包類型是否為 RANGING_INITIATION
-//         if (recv_data[15] == RANGING_INITIATION) {
-//             // 檢查數據包中的短地址是否與當前 tag 的短地址匹配
-//             if (recv_data[16] == tag_short_address[0] && recv_data[17] == tag_short_address[1]) {
-//                 // 當前 tag 是目標 tag，進行相應的處理
-//                 Serial.println("Ranging initiation received for this tag.");
-//                 // 進行測距請求
-//                 DW1000NgRTLS::transmitPoll(main_anchor_address);
-//                 DW1000NgRTLS::waitForTransmission();
-                
-//                 // 等待測距響應
-//                 if (DW1000NgRTLS::receiveFrame()) {
-//                     size_t response_len = DW1000Ng::getReceivedDataLength();
-//                     byte response_data[response_len];
-//                     DW1000Ng::getReceivedData(response_data, response_len);
-
-//                     // 檢查數據包類型是否為 RANGING_RESPONSE
-//                     if (response_data[9] == RANGING_RESPONSE) {
-//                         // 處理測距響應
-//                         Serial.println("Ranging response received.");
-//                         // 計算距離並顯示
-//                         double range = calculateRange(response_data);
-//                         Serial.print("Range: ");
-//                         Serial.print(range);
-//                         Serial.println(" m");
-//                     }
-//                 }
-//             }
-//         }
-//     }
