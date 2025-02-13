b17d268663af75930ef4655996530f6cb3600d5b
Enfu Liao <enfu.liao.cs10@nycu.edu.tw>
2025-02-13 18:49:53 +0000

    feat: add uwb ros2 pkg

---
+++ ros/build/uwb_msgs/CMakeFiles/uwb_msgs__rosidl_generator_c.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uwb_msgs__rosidl_generator_c.dir/rosidl_generator_c/uwb_msgs/msg/detail/range__functions.c.o"
+  "CMakeFiles/uwb_msgs__rosidl_generator_c.dir/rosidl_generator_c/uwb_msgs/msg/detail/range__functions.c.o.d"
+  "libuwb_msgs__rosidl_generator_c.pdb"
+  "libuwb_msgs__rosidl_generator_c.so"
+  "rosidl_generator_c/uwb_msgs/msg/detail/range__functions.c"
+  "rosidl_generator_c/uwb_msgs/msg/detail/range__functions.h"
+  "rosidl_generator_c/uwb_msgs/msg/detail/range__struct.h"
+  "rosidl_generator_c/uwb_msgs/msg/detail/range__type_support.h"
+  "rosidl_generator_c/uwb_msgs/msg/range.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/uwb_msgs__rosidl_generator_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ros/build/uwb_msgs/CMakeFiles/uwb_msgs__rosidl_typesupport_c.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uwb_msgs__rosidl_typesupport_c.dir/rosidl_typesupport_c/uwb_msgs/msg/range__type_support.cpp.o"
+  "CMakeFiles/uwb_msgs__rosidl_typesupport_c.dir/rosidl_typesupport_c/uwb_msgs/msg/range__type_support.cpp.o.d"
+  "libuwb_msgs__rosidl_typesupport_c.pdb"
+  "libuwb_msgs__rosidl_typesupport_c.so"
+  "rosidl_typesupport_c/uwb_msgs/msg/range__type_support.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uwb_msgs__rosidl_typesupport_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
