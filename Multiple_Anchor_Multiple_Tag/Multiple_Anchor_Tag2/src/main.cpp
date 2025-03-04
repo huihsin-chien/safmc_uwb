9763d91c8f9a5561d9dd8c14a67beafd7f41d127
popisgood <popgadeedee@gmail.com>
2025-03-05 01:03:12 +0800

    change UWB configuration to channel 4 and etc.

--- Multiple_Anchor_Multiple_Tag/Multiple_Anchor_Tag2/src/main.cpp
+++ Multiple_Anchor_Multiple_Tag/Multiple_Anchor_Tag2/src/main.cpp
@@ -14,11 +14,11 @@ const uint8_t PIN_RST = 15;
 device_configuration_t DEFAULT_CONFIG = {
     false, true, true, true, false,
     SFDMode::STANDARD_SFD,
-    Channel::CHANNEL_5,
+    Channel::CHANNEL_4,
     DataRate::RATE_850KBPS,
-    PulseFrequency::FREQ_16MHZ,
-    PreambleLength::LEN_256,
-    PreambleCode::CODE_3
+    PulseFrequency::FREQ_64MHZ,
+    PreambleLength::LEN_1024,
+    PreambleCode::CODE_4
 };
 
 frame_filtering_configuration_t TAG_FRAME_FILTER_CONFIG = {
