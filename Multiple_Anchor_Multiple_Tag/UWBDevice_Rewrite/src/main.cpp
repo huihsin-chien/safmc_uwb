e8e078849a9f4d7aa9feee0c2c5ada638723d517
XiaoPanPanKevinPan <XiaoPanPanKevinPan@users.noreply.github.com>
2025-03-14 05:37:09 +0800

    [mod] simplify UWBDevice_Rewrite conf

--- Multiple_Anchor_Multiple_Tag/UWBDevice_Rewrite/src/main.cpp
+++ Multiple_Anchor_Multiple_Tag/UWBDevice_Rewrite/src/main.cpp
@@ -3,14 +3,15 @@
 // == START OF Device Config ==
 #define TAG_1
 
+// const char becomeTagSuccessMessage[] = "";
+// const char becomeAnchorSuccessMessage[] = "";
+
 #ifdef ANCHOR_1
 const char EUI[] = "AA:BB:CC:DD:EE:FF:00:01";
 const uint16_t self_device_address = 0x0001;
 const bool isAnchorByDefault = true;
 const char becomeTagSymbols[] = "";
 const char becomeAnchorSymbols[] = "";
-const char becomeTagSuccessMessage[] = "";
-const char becomeAnchorSuccessMessage[] = "";
 #endif
 
 #ifdef ANCHOR_2
@@ -19,8 +20,6 @@ const uint16_t self_device_address = 0x0002;
 const bool isAnchorByDefault = false;
 const char becomeTagSymbols[] = "1";
 const char becomeAnchorSymbols[] = "234567f";
-const char becomeTagSuccessMessage[] = "";
-const char becomeAnchorSuccessMessage[] = "";
 #endif
 
 #ifdef ANCHOR_3
@@ -29,8 +28,6 @@ const uint16_t self_device_address = 0x0003;
 const bool isAnchorByDefault = false;
 const char becomeTagSymbols[] = "12";
 const char becomeAnchorSymbols[] = "34567f";
-const char becomeTagSuccessMessage[] = "";
-const char becomeAnchorSuccessMessage[] = "";
 #endif
 
 #ifdef ANCHOR_4
@@ -39,8 +36,6 @@ const uint16_t self_device_address = 0x0004;
 const bool isAnchorByDefault = false;
 const char becomeTagSymbols[] = "123";
 const char becomeAnchorSymbols[] = "4567f";
-const char becomeTagSuccessMessage[] = "";
-const char becomeAnchorSuccessMessage[] = "";
 #endif
 
 #ifdef ANCHOR_5
@@ -49,8 +44,6 @@ const uint16_t self_device_address = 0x0005;
 const bool isAnchorByDefault = false;
 const char becomeTagSymbols[] = "1234";
 const char becomeAnchorSymbols[] = "5f";
-const char becomeTagSuccessMessage[] = "";
-const char becomeAnchorSuccessMessage[] = "";
 #endif
 
 #ifdef ANCHOR_6
@@ -59,8 +52,6 @@ const uint16_t self_device_address = 0x0006;
 const bool isAnchorByDefault = false;
 const char becomeTagSymbols[] = "1234";
 const char becomeAnchorSymbols[] = "6f";
-const char becomeTagSuccessMessage[] = "";
-const char becomeAnchorSuccessMessage[] = "";
 #endif
 
 #ifdef ANCHOR_7
@@ -69,8 +60,6 @@ const uint16_t self_device_address = 0x0007;
 const bool isAnchorByDefault = false;
 const char becomeTagSymbols[] = "1234";
 const char becomeAnchorSymbols[] = "7f";
-const char becomeTagSuccessMessage[] = "";
-const char becomeAnchorSuccessMessage[] = "";
 #endif
 
 #ifdef ANCHOR_8
@@ -79,89 +68,55 @@ const uint16_t self_device_address = 0x0008;
 const bool isAnchorByDefault = false;
 const char becomeTagSymbols[] = "1234";
 const char becomeAnchorSymbols[] = "8f";
-const char becomeTagSuccessMessage[] = "";
-const char becomeAnchorSuccessMessage[] = "";
+#endif
+
+#if defined(TAG_1) || defined(TAG_2) || defined(TAG_3) || defined(TAG_4) \
+    || defined(TAG_5) || defined(TAG_6) || defined(TAG_7) || defined(TAG_8)
+const bool isAnchorByDefault = false;
+const char becomeTagSymbols[] = "";
+const char becomeAnchorSymbols[] = "";
 #endif
 
 #ifdef TAG_1
 const char EUI[] = "AA:BB:CC:DD:EE:FF:01:01";
 const uint16_t self_device_address = 0x0101;
-const bool isAnchorByDefault = false;
-const char becomeTagSymbols[] = "";
-const char becomeAnchorSymbols[] = "";
-const char becomeTagSuccessMessage[] = "";
-const char becomeAnchorSuccessMessage[] = "";
 #endif
 
 #ifdef TAG_2
 const char EUI[] = "AA:BB:CC:DD:EE:FF:02:02";
 const uint16_t self_device_address = 0x0202;
-const bool isAnchorByDefault = false;
-const char becomeTagSymbols[] = "";
-const char becomeAnchorSymbols[] = "";
-const char becomeTagSuccessMessage[] = "";
-const char becomeAnchorSuccessMessage[] = "";
 #endif
 
 #ifdef TAG_3
 const char EUI[] = "AA:BB:CC:DD:EE:FF:03:03";
 const uint16_t self_device_address = 0x0303;
-const bool isAnchorByDefault = false;
-const char becomeTagSymbols[] = "";
-const char becomeAnchorSymbols[] = "";
-const char becomeTagSuccessMessage[] = "";
-const char becomeAnchorSuccessMessage[] = "";
 #endif
 
 #ifdef TAG_4
 const char EUI[] = "AA:BB:CC:DD:EE:FF:04:04";
 const uint16_t self_device_address = 0x0404;
-const bool isAnchorByDefault = false;
-const char becomeTagSymbols[] = "";
-const char becomeAnchorSymbols[] = "";
-const char becomeTagSuccessMessage[] = "";
-const char becomeAnchorSuccessMessage[] = "";
 #endif
 
 #ifdef TAG_5
 const char EUI[] = "AA:BB:CC:DD:EE:FF:05:05";
 const uint16_t self_device_address = 0x0505;
-const bool isAnchorByDefault = false;
-const char becomeTagSymbols[] = "";
-const char becomeAnchorSymbols[] = "";
-const char becomeTagSuccessMessage[] = "";
-const char becomeAnchorSuccessMessage[] = "";
 #endif
 
 #ifdef TAG_6
 const char EUI[] = "AA:BB:CC:DD:EE:FF:06:06";
 const uint16_t self_device_address = 0x0606;
-const bool isAnchorByDefault = false;
-const char becomeTagSymbols[] = "";
-const char becomeAnchorSymbols[] = "";
-const char becomeTagSuccessMessage[] = "";
-const char becomeAnchorSuccessMessage[] = "";
 #endif
 
 #ifdef TAG_7
 const char EUI[] = "AA:BB:CC:DD:EE:FF:07:07";
 const uint16_t self_device_address = 0x0707;
-const bool isAnchorByDefault = false;
-const char becomeTagSymbols[] = "";
-const char becomeAnchorSymbols[] = "";
-const char becomeTagSuccessMessage[] = "";
-const char becomeAnchorSuccessMessage[] = "";
 #endif
 
 #ifdef TAG_8
 const char EUI[] = "AA:BB:CC:DD:EE:FF:08:08";
 const uint16_t self_device_address = 0x0808;
-const bool isAnchorByDefault = false;
-const char becomeTagSymbols[] = "";
-const char becomeAnchorSymbols[] = "";
-const char becomeTagSuccessMessage[] = "";
-const char becomeAnchorSuccessMessage[] = "";
 #endif
+
 // ==  END OF Device Config  ==
 
 uint32_t blink_rate = 50;
@@ -196,7 +151,7 @@ void try_to_change_state() {
                 if(ch == becomeTagSymbols[i]) {
                     isAnchor = false;
                     DW1000Ng::enableFrameFiltering(TAG_FRAME_FILTER_CONFIG);
-                    Serial.print(becomeTagSuccessMessage);
+                    // Serial.print(becomeTagSuccessMessage);
                 }
             }
         } else {
@@ -204,7 +159,7 @@ void try_to_change_state() {
                 if(ch == becomeAnchorSymbols[i]) {
                     isAnchor = true;
                     DW1000Ng::enableFrameFiltering(ANCHOR_FRAME_FILTER_CONFIG);
-                    Serial.print(becomeAnchorSuccessMessage);
+                    // Serial.print(becomeAnchorSuccessMessage);
                 }
             }
         }
