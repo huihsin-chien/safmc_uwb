1ea3926805eaa88a5fcba22d9473c9153d232a39
jianhuixin <comet112511092.ee12@nycu.edu.tw>
2025-04-17 21:37:31 +0800

    add TODO comment

--- wirelessUWB/wirelessUWB/src/DW1000NgRTLS_ext.cpp
+++ wirelessUWB/wirelessUWB/src/DW1000NgRTLS_ext.cpp
@@ -348,7 +348,7 @@ namespace DW1000NgRTLS_ext {
     // Range: 9.04 m	 RX power: -82.30 dBm distance between anchor/tag:40 from Anchor 00:01
 
     // tag_range： 取小數點後 2 位數，範圍可能為 0.01 ~ 500.00，留 5 個 byte 
-    // RX_power：  取小數點後 2 位數，範圍可能為 -128.00 ~ 0.00，留 5 個 byte
+    // RX_power：  取小數點後 2 位數，範圍可能為 -128.00 ~ 0.00，留 5 個 byte TODO 需確刃
     // 經由 transmitPoll() 修改成 transmitDataToMediatorUWB
     void transmitDataToMediatorUWB(byte mediatorUWB_address[], byte tagDeviceAddress[], double tag_range, float RX_power) {
         // byte tag_range_arr[5];
@@ -359,9 +359,10 @@ namespace DW1000NgRTLS_ext {
         RX_power = abs(RX_power);
         tag_range *= 100; // 將 tag_range 乘上 100，使其成為整數，使用 writeValueToBytes
         RX_power *= 100; 
-        static_cast<uint16_t>(tag_range);
+        static_cast<uint16_t>(tag_range); // 能用uint16_t嗎?
         static_cast<uint16_t>(RX_power);
 
+        // TODO 要不要改成 16 進位
         byte Poll[] = {DATA, SHORT_SRC_AND_DEST, SEQ_NUMBER++, 0,0, 0,0, 0,0 , RANGING_TAG_POLL, 
             tagDeviceAddress[0], tagDeviceAddress[1], 0,0,0,0,0, 0,0,0,0,0,};
         DW1000NgUtils::writeValueToBytes(&Poll[10], tag_range, 5);
