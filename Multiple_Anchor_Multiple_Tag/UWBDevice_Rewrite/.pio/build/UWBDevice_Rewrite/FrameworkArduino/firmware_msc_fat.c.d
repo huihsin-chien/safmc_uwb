6d91cf80a316f34e34262fa6d15187e182163201
unknown <judyliang.cs13@nycu.edu.tw>
2025-05-20 21:39:58 +0800

    Add selected files for clean branch

---
+++ Multiple_Anchor_Multiple_Tag/UWBDevice_Rewrite/.pio/build/UWBDevice_Rewrite/FrameworkArduino/firmware_msc_fat.c.d
@@ -0,0 +1,3 @@
+.pio/build/UWBDevice_Rewrite/FrameworkArduino/firmware_msc_fat.c.o: \
+ C:/Users/Judy/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
+ C:/Users/Judy/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
