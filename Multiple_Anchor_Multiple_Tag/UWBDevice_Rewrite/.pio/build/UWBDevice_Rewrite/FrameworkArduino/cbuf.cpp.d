58401ea674066d4ea210a9549e4cb113dfc190a3
Judy Pumpkin <95557193+Judy0Pumpkin@users.noreply.github.com>
2025-05-20 21:45:11 +0800

    Delete Multiple_Anchor_Multiple_Tag/UWBDevice_Rewrite directory

--- Multiple_Anchor_Multiple_Tag/UWBDevice_Rewrite/.pio/build/UWBDevice_Rewrite/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/UWBDevice_Rewrite/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/Judy/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/Judy/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
--- Multiple_Anchor_Multiple_Tag/UWBDevice_Rewrite/.pio/build/UWBDevice_Rewrite/FrameworkArduino/libb64/cdecode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/UWBDevice_Rewrite/FrameworkArduino/libb64/cdecode.c.o: \
- C:/Users/Judy/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.c \
- C:/Users/Judy/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.h
